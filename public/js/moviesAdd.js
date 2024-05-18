2919782ba51540de1f1b2f225d3cfca2963ab70d
AndrewFlowe-rs <arnaldomahone@gmail.com>
2024-05-17 21:44:28 -0300

    se termina el desafio

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -1,12 +1,75 @@
-window.onload = function(){
-    let titulo = document.querySelector('.moviesAddTitulo')
-    let formulario = document.querySelector('#formulario');
-    let article = document.querySelector('article');
-    titulo.innerHTML = 'AGREGAR PELÍCULA';
-    titulo.classList.add('titulo');
-    article.classList.add('fondoTransparente');
-    formulario.classList.add('fondoCRUD');
+alert("bienvenido a la vista de agregar una pelicula");
+window.onload = function () {
+  let titulo = document.querySelector(".moviesAddTitulo");
+  let formulario = document.querySelector("#formulario");
+  let article = document.querySelector("article");
+  let agregarPelicula = document.querySelector(".botonAgregar");
+  let imputTitle = document.querySelector("#titulo");
+
+  titulo.innerHTML = "AGREGAR PELÍCULA";
+  titulo.classList.add("titulo");
+  article.classList.add("fondoTransparente");
+  formulario.classList.add("fondoCRUD");
+
+  titulo.addEventListener("mouseover", () => {
+    titulo.style.color = "red";
+    agregarPelicula.style.backgroundColor = "white";
+    agregarPelicula.style.color = "red";
+  });
+  titulo.addEventListener("mouseout", () => {
+    agregarPelicula.style.backgroundColor = "green";
+    agregarPelicula.style.color = "white";
+    titulo.style.color = "black";
+  });
+
+imputTitle.style.color= 'red'
+
+let estadoSecreto = '0'
+// Escucha el evento de presionar una tecla en el input
+imputTitle.addEventListener('keydown', (event) => {
+    const teclaPresionada = event.key.toLowerCase();
 
 
-
-}
+    switch (estadoSecreto) {
+        case 0:
+            if (teclaPresionada === 's') {
+                estadoSecreto = 1;
+            }
+            break;
+        case 1:
+            if (teclaPresionada === 'e') {
+                estadoSecreto = 2;
+            }
+            break;
+        case 2:
+            if (teclaPresionada === 'c') {
+                estadoSecreto = 3;
+            }
+            break;
+        case 3:
+            if (teclaPresionada === 'r') {
+                estadoSecreto = 4;
+            }
+            break;
+        case 4:
+            if (teclaPresionada === 'e') {
+                estadoSecreto = 5;
+            }
+            break;
+        case 5:
+            if (teclaPresionada === 't') {
+                estadoSecreto = 6;
+            }
+            break;
+        case 6:
+            if (teclaPresionada === 'o') {
+                estadoSecreto = 0;
+                alert('SECRETO MAGICO');
+            }
+            break;
+        default:
+            estadoSecreto = 0;
+            break;
+    }
+});
+};
--- public/js/moviesList.js
+++ public/js/moviesList.js
@@ -1,22 +1,22 @@
-window.onload = function(){
-    let body = document.querySelector('body');
-    let moviesListTitulo = document.querySelector('.moviesListTitulo');
-    let logoDH = document.querySelector('figure');
-    logoDH.addEventListener('mouseover', function(){
-        let modo = confirm('Desea modo oscuro');
-        if(modo){
-            body.style.backgroundColor = '#7f7f7f'
-            body.classList.add('fondoMoviesList');
-        }
-      
-    })
-    
-   
-    
-    console.log(body);
-    moviesListTitulo.innerHTML = 'LISTADO DE PELÍCULAS';
-    moviesListTitulo.style.color ='white';
-    moviesListTitulo.style.backgroundColor = 'teal';
-    moviesListTitulo.style.padding = '20px';
+window.onload = function () {
+  let body = document.querySelector("body");
+  let moviesListTitulo = document.querySelector(".moviesListTitulo");
+  let logoDH = document.querySelector("figure");
+  logoDH.addEventListener("mouseover", function () {
+     let modo = confirm("Desea modo oscuro");
+     if (modo) {
+      body.style.backgroundColor = "#7f7f7f";
+      body.classList.add("fondoMoviesList");
+     } else {
+       body.classList.remove("fondoMoviesList");
+       body.style.backgroundColor = "white";
+     }
 
-}
+   });
+
+  console.log(body);
+  moviesListTitulo.innerHTML = "LISTADO DE PELÍCULAS";
+  moviesListTitulo.style.color = "white";
+  moviesListTitulo.style.backgroundColor = "teal";
+  moviesListTitulo.style.padding = "20px";
+};
